e74402c46eea8ea92b3db0b5d920e89407d9cf70
erinestafford <estaffor@tulane.edu>
2017-07-05 16:34:17 -0700

    updated params

--- Saguaro model/SaguaroSim.m
+++ Saguaro model/SaguaroSim.m
@@ -1,13 +1,14 @@
 %% Params
-params= struct('phi', 0.1,'sigma', .65,'gamma',1/35, 'beta', 1/175, 'b', .01, 'd', 1/5,'k',.05, 'c', .02);
-
-s_j0 = 4;
-s_a0 = 10;
-t0 = 6;
-
+s_j0 = 400;
+s_a0 = 1000;
+t0 = 600;
+params= struct('phi',300,'delta', .85,'gamma',1/35, 'beta', 1/145, 'b', .01, 'd', 1/150,'k',.05, 'c', .02);
+%phi is seeds that are germinated per year
+%delta is the natural death rate of the young saguaros without the
+%protection of host trees
 %% Simulation
  y0 = [s_j0 s_a0 t0];
- tspan = [0:10:200];
+ tspan = [0 20];
  fn = @(t,y)basicLifeCycle(t,y,params);
  [T,Y] = ode45(fn, tspan, y0);
  figure('DefaultAxesFontSize', 12)
