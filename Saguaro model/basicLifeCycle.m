7b0496c9acbfdadadf52fcf80d377289614cf6fd
erinestafford <estaffor@tulane.edu>
2017-07-05 20:50:32 -0700

    updated model

--- Saguaro model/basicLifeCycle.m
+++ Saguaro model/basicLifeCycle.m
@@ -1,6 +1,6 @@
 function pops = basicLifeCycle(t,y,params)
-pops(1) = params.phi*y(2) - params.gamma*y(1) - (params.delta*y(1)/(1+y(3)));
-pops(2) = params.gamma*y(1) - params.beta*y(2);
-pops(3) = params.b*y(3) - params.d*y(3) - (params.k *y(2)+params.c*y(3))*y(3)/params.k;
+pops(1) = params.r1*y(2)*(1-(y(2)/params.k1+params.b*y(3))) - params.gamma*y(1) - params.muj*y(1);
+pops(2) = params.gamma*y(1) - params.alpha1*y(3)*params.r2/params.k2 - params.mua*y(2);
+pops(3) = params.r3*y(3)*((params.k3-y(3)-params.alpha2*y(2))/params.k3) - params.mut * y(3);
 pops=pops';
 end
