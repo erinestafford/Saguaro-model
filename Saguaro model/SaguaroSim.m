6e31774ca860e0f78a0195ce56323068642d9d62
erinestafford <estaffor@tulane.edu>
2017-07-05 23:59:08 -0700

    fixed negative problem

--- Saguaro model/SaguaroSim.m
+++ Saguaro model/SaguaroSim.m
@@ -1,27 +1,27 @@
+clear all; close all;
 %% Params
-s_j0 = 400;
+s_j0 = 2000;
 s_a0 = 100;
-t0 = 600;
+t0 = 1500;
 param_struct= ...
-{'r1',.005;
+{'r1',.05;
 'k1', 3000;
-'b',.75;
+'b',.95;
 'gamma', 1/35;
-'c', .00001;
-'phi',.993;
+'phi',0.005;
 'A',1000;
 'muj', 0.005;
 'mua',1/145;
-'alpha1', .6;
-'alpha2',0.95;
+'alpha1', .000001;
+'alpha2',0.85;
 }';
 params = struct(param_struct{:});
 
 %% Simulation
  y0 = [s_j0 s_a0 t0];
- tspan = [0 200];
+ tspan = [0 500];
  fn = @(t,y)basicLifeCycle(t,y,params);
- [T,Y] = ode45(fn, tspan, y0);
+ [T,Y] = ode23s(fn, tspan, y0);
  figure('DefaultAxesFontSize', 12)
  plot(T,Y(:,1),'LineWidth', 2);
  hold on
--- Saguaro model/basicLifeCycle.m
+++ Saguaro model/basicLifeCycle.m
@@ -1,6 +1,7 @@
 function pops = basicLifeCycle(t,y,params)
-pops(1) = params.r1*y(2)*(1-(y(2)/params.k1+params.b*y(3))) - params.gamma*y(1) - params.muj*y(1);
-pops(2) = params.gamma*y(1) - params.mua*y(2)- params.c * y(2) * y(3);
-pops(3) = params.phi * (1 - (params.alpha2 * y(2) + y(3))/params.A);
+
+pops(1) = params.r1*y(2)*(1-(y(2)/(params.k1+params.b*y(3)))) - params.gamma*y(1) - params.muj*y(1);
+pops(2) = params.gamma*y(1) - params.mua*y(2)- (params.alpha1/params.A) * y(2) * y(3);
+pops(3) = params.phi * y(3)*(1 - (params.alpha2 * y(2) + y(3))/params.A);
 pops=pops';
 end
