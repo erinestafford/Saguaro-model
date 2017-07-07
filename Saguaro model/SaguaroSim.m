1c10768017a2b5de6dc4f97a5f4c25590dc46f27
erinestafford <estaffor@tulane.edu>
2017-07-07 10:25:30 -0700

    Seasonality Graphs

--- Saguaro model/SaguaroSim.m
+++ Saguaro model/SaguaroSim.m
@@ -2,15 +2,16 @@ clear all; close all;
 %% Params
 s_j0 = 300;
 s_a0 = 1100;
-t0 = 100;
+t0 = 1000;
+space = 10000; %1 hectare, in meters
 param_struct= ...
-{'r1',(375*0.001);%make function of time?
-'k1', 1000;
+{'r1',(375*0.0126);%make function of time?
+'k1', 500;
 'b',.85;
 'gamma', 1/35;
 'phi',(0.05-1/100);
-'A',2000;
-'muj', .5;
+'A',1000;
+'muj', .25;
 'mua',1/50;
 'alpha1', .000001;
 'alpha2',0.95;
@@ -20,7 +21,7 @@ params = struct(param_struct{:});
 
 %% Simulation
  y0 = [s_j0 s_a0 t0];
- tspan = [0 500];
+ tspan = [0 400];
  fn = @(t,y)basicLifeCycle(t,y,params);
  [T,Y] = ode23s(fn, tspan, y0);
  figure('DefaultAxesFontSize', 12)
--- Saguaro model/testRates.m
+++ Saguaro model/testRates.m
@@ -1,3 +1,13 @@
 close all;
 tspan = [0:.001:1];
-plot(tspan,calc_muj(tspan))
+subplot(1,2,1)
+plot(tspan,calc_r1(tspan))
+xlabel('Time, Over One Year')
+ylabel('Saguaro Germination Rate')
+title('Seasonal Germination Rate')
+
+subplot(1,2,2)
+plot(tspan,calc_mua(tspan))
+xlabel('Time, Over One Year')
+ylabel('Saguaro Death Rate')
+title('Seasonal Death Rate')
